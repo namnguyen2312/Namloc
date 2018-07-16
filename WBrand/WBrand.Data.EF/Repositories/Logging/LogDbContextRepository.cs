83146896588c09fb4e75a1de8344ca895d6158ad
TanNguyen <itnbtan@gmail.com>
2018-07-16 22:04:52 +0700

    change name folder log to logging

---
+++ WBrand/WBrand.Data.EF/Repositories/Logging/LogDbContextRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WBrand.Core.Domain.Entities.System;
+using WBrand.Data.Logging;
+
+namespace WBrand.Data.EF.Repositories.Log
+{
+    public class LogDbContextRepository : BaseRepository<LogDbContext>, ILogDbContextRepository
+    {
+        public LogDbContextRepository(WBrandDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
