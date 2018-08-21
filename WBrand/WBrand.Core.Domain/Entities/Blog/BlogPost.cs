701b86480fddd38f129da36cc663b8920a9a6c1d
tannb <itnbtan@gmail.com>
2018-08-21 22:38:26 +0700

    add blog post

---
+++ WBrand/WBrand.Core.Domain/Entities/Blog/BlogPost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBrand.Core.Domain.Entities.Blog
+{
+    public class BlogPost : SeoEntity<long>
+    {
+        [MaxLength(128)]
+        public string Name { set; get; }
+
+        [MaxLength(128)]
+        public string ShortContent { set; get; }
+
+        [MaxLength(128)]
+        public string FullContent { set; get; }
+
+        public DateTimeOffset PublishDate { set; get; }
+
+        public int CategoryId { set; get; }
+        
+    }
+}
